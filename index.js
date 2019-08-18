492555440fba053d5ab227855e1bf1e8df2be94a
Josh Turan <joshturan@Joshs-Air.lan>
2019-08-18 19:51:34 +0200

    Updated through 3.3

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('working')
+const express = require('express')
+const app = express()
+
+let persons = [
+  { id: 1, name: 'Arto Hellas', number: '040-123456' },
+  { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+  { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+  { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
+]
+
+app.get('/api', (req, res) => {
+  res.send('<h1>Hello API!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+
+  console.log(response)
+})
+
+app.get('/info', (req, res) => {
+  const date = new Date()
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}<p>`
+  )
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
