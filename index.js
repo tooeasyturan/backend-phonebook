18e690d30fd21ac8b51ad5faf87fe74d33c1e00c
Josh Turan <joshturan@Joshs-Air.lan>
2019-08-21 21:08:32 +0200

    Prepare heroku deploy, add procfile and gitignore

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
+app.use(cors())
+
 
 
 app.use(bodyParser.json())
@@ -85,7 +88,7 @@ app.delete('/api/persons/:id', (request, response) => {
 //morgan(':method :url :status :res[content-length] - :response-time ms')
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
